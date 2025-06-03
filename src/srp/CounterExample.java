efd4786a86e2e60963ad8f9268e4f6153396c711
Mohamed Diop <mohdiopcode@gmail.com>
2025-06-03 10:30:03 +0000

    Ajoute le contrexemple de SRP

---
+++ src/srp/CounterExample.java
@@ -0,0 +1,27 @@
+package srp;
+
+public class CounterExample {
+    // Tout dans une même classe EmployeeManager
+    static class EmployeeManager{
+        private String name;
+        private Double salary;
+        public EmployeeManager(String name, Double salary){
+            this.name = name;
+            this.salary = salary;
+        }
+        public String getName(){
+            return name;
+        }
+        public Double getSalary() {
+            return salary;
+        }
+        public Double calculateAnnualSalary(Employee e){
+            return e.getSalary() * 12;
+        }
+        public void printReport(Employee e, Double annualSalary){
+            System.out.println("Rapport de l'employé : ");
+            System.out.println("Nom                  : " + e.getName() + ".");
+            System.out.println("Salaire annuel       : " + annualSalary.toString() + " FCFA.");
+        }
+    }
+}
